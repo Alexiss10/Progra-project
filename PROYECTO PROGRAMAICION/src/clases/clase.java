62e01f9c2b8198e5922bf88a972370f88e5d8aee
Alexiss10 <alexissantonio26@gmail.com>
2026-09-08 14:11:31 -0600

    Subiendo cambio

--- PROYECTO PROGRAMAICION/src/clases/clase.java
+++ PROYECTO PROGRAMAICION/src/clases/clase.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package clases;
 
 /**
@@ -10,11 +9,9 @@ package clases;
  * @author ALEXIS
  */
 public class clase {
+
     public static void main(String[] args) {
         System.out.println("Hola, como estas");
-        System.out.println("Hola mundo");
-        System.out.println("-----------");
-        CLASE2 class2 = new CLASE2();
-        class2.imprimirClase2();
+        System.out.println("Hola mundo");    
     }
 }
